f51c9af7e5585b2c8e2209e2a89e817295455eb5
0x1B05 <liuheihei677@163.com>
2023-07-16 09:12:59 +0800

    Add Comment.nvim and modify gitsign.

--- nvim/.config/nvim/lua/plugins/00_generic.lua
+++ nvim/.config/nvim/lua/plugins/00_generic.lua
@@ -90,6 +90,69 @@ return {
 			"nvim-lua/plenary.nvim",
 			-- "kyazdani42/nvim-web-devicons",
 		},
+		opts = {
+			signs = {
+				add = { hl = "GitSignsAdd", text = " ", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+				change = {
+					hl = "GitSignsChange",
+					text = " ",
+					numhl = "GitSignsChangeNr",
+					linehl = "GitSignsChangeLn",
+				},
+				delete = {
+					hl = "GitSignsDelete",
+					text = " ",
+					numhl = "GitSignsDeleteNr",
+					linehl = "GitSignsDeleteLn",
+				},
+				topdelete = {
+					hl = "GitSignsDelete",
+					text = "󱅁 ",
+					numhl = "GitSignsDeleteNr",
+					linehl = "GitSignsDeleteLn",
+				},
+				changedelete = {
+					hl = "GitSignsChange",
+					text = "󰍷 ",
+					numhl = "GitSignsChangeNr",
+					linehl = "GitSignsChangeLn",
+				},
+			},
+			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+			watch_gitdir = {
+				interval = 1000,
+				follow_files = true,
+			},
+			attach_to_untracked = true,
+			current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 1000,
+				ignore_whitespace = false,
+			},
+			current_line_blame_formatter_opts = {
+				relative_time = false,
+			},
+			sign_priority = 6,
+			update_debounce = 100,
+			status_formatter = nil, -- Use default
+			max_file_length = 40000,
+			preview_config = {
+				-- Options passed to nvim_open_win
+				border = "single",
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1,
+			},
+			yadm = {
+				enable = false,
+			},
+		},
 		config = function()
 			require("gitsigns").setup({})
 		end,
@@ -113,47 +176,39 @@ return {
 			})
 		end,
 	},
-	-- auto pairs
-	{
-		"echasnovski/mini.pairs",
-		event = "VeryLazy",
-		config = function(_, opts)
-			require("mini.pairs").setup(opts)
-		end,
-	},
 	-- project manager
 	{
 		"ahmedkhalf/project.nvim",
 		lazy = false,
-        opts = {
-				---@usage set to false to disable project.nvim.
-				--- This is on by default since it's currently the expected behavior.
-				active = true,
-				on_config_done = nil,
-				---@usage set to true to disable setting the current-woriking directory
-				--- Manual mode doesn't automatically change your root directory, so you have
-				--- the option to manually do so using `:ProjectRoot` command.
-				manual_mode = false,
-				---@usage Methods of detecting the root directory
-				--- Allowed values: **"lsp"** uses the native neovim lsp
-				--- **"pattern"** uses vim-rooter like glob pattern matching. Here
-				--- order matters: if one is not detected, the other is used as fallback. You
-				--- can also delete or rearangne the detection methods.
-				-- detection_methods = { "lsp", "pattern" }, -- NOTE: lsp detection will get annoying with multiple langs in one project
-				detection_methods = { "lsp", "pattern" },
-				---@usage patterns used to detect root dir, when **"pattern"** is in detection_methods
-				patterns = { ".git", "CMakeLists.txt", ".svn", "Makefile", "package.json" },
-				---@ Show hidden files in telescope when searching for files in a project
-				show_hidden = false,
-				---@usage When set to false, you will get a message when project.nvim changes your directory.
-				-- When set to false, you will get a message when project.nvim changes your directory.
-				silent_chdir = true,
-				---@usage list of lsp client names to ignore when using **lsp** detection. eg: { "efm", ... }
-				ignore_lsp = {},
-				---@type string
-				---@usage path to store the project history for use in telescope
-				datapath = vim.fn.stdpath("data"),
-			},
+		opts = {
+			---@usage set to false to disable project.nvim.
+			--- This is on by default since it's currently the expected behavior.
+			active = true,
+			on_config_done = nil,
+			---@usage set to true to disable setting the current-woriking directory
+			--- Manual mode doesn't automatically change your root directory, so you have
+			--- the option to manually do so using `:ProjectRoot` command.
+			manual_mode = false,
+			---@usage Methods of detecting the root directory
+			--- Allowed values: **"lsp"** uses the native neovim lsp
+			--- **"pattern"** uses vim-rooter like glob pattern matching. Here
+			--- order matters: if one is not detected, the other is used as fallback. You
+			--- can also delete or rearangne the detection methods.
+			-- detection_methods = { "lsp", "pattern" }, -- NOTE: lsp detection will get annoying with multiple langs in one project
+			detection_methods = { "lsp", "pattern" },
+			---@usage patterns used to detect root dir, when **"pattern"** is in detection_methods
+			patterns = { ".git", "CMakeLists.txt", ".svn", "Makefile", "package.json" },
+			---@ Show hidden files in telescope when searching for files in a project
+			show_hidden = false,
+			---@usage When set to false, you will get a message when project.nvim changes your directory.
+			-- When set to false, you will get a message when project.nvim changes your directory.
+			silent_chdir = true,
+			---@usage list of lsp client names to ignore when using **lsp** detection. eg: { "efm", ... }
+			ignore_lsp = {},
+			---@type string
+			---@usage path to store the project history for use in telescope
+			datapath = vim.fn.stdpath("data"),
+		},
 		config = function()
 			require("telescope").load_extension("projects")
 		end,
--- nvim/.config/nvim/lua/plugins/04_coding.lua
+++ nvim/.config/nvim/lua/plugins/04_coding.lua
@@ -89,17 +89,74 @@ return {
 	-- comments
 	{ "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
 	{
-		"echasnovski/mini.comment",
-		event = "VeryLazy",
-		opts = {
-			hooks = {
-				pre = function()
-					require("ts_context_commentstring.internal").update_commentstring({})
-				end,
-			},
+		"numToStr/Comment.nvim",
+		lazy = true,
+		keys = {
+			{ "gcc", mode = "n", desc = "Line-comment toggle keymap" },
+			{ "gbc", mode = "n", desc = "Block-comment toggle keymap" },
+			{ "gbO", mode = "n", esc = "Add comment on the line above" },
+			{ "gbo", mode = "n", esc = "Add comment on the line below" },
+			{ "gbA", mode = "n", esc = "Add comment at the end of line" },
 		},
-		config = function(_, opts)
-			require("mini.comment").setup(opts)
+		config = function()
+			require("Comment").setup({
+				---Add a space b/w comment and the line
+				padding = true,
+				---Whether the cursor should stay at its position
+				sticky = true,
+				---Lines to be ignored while (un)comment
+				ignore = nil,
+				---LHS of toggle mappings in NORMAL mode
+				toggler = {
+					line = "gcc",
+					block = "gbc",
+				},
+				---LHS of operator-pending mappings in NORMAL and VISUAL mode
+				opleader = {
+					line = "gc",
+					block = "gb",
+				},
+				---LHS of extra mappings
+				extra = {
+					above = "gcO",
+					below = "gco",
+					---Add comment at the end of line
+					eol = "gcA",
+				},
+				---Enable keybindings
+				---NOTE: If given `false` then the plugin won't create any mappings
+				mappings = {
+					---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+					basic = true,
+					---Extra mapping; `gco`, `gcO`, `gcA`
+					extra = true,
+				},
+				pre_hook = function(ctx)
+					local U = require("Comment.utils")
+
+					local status_utils_ok, utils = pcall(require, "ts_context_commentstring.utils")
+					if not status_utils_ok then
+						return
+					end
+
+					local location = nil
+					if ctx.ctype == U.ctype.block then
+						location = utils.get_cursor_location()
+					elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+						location = utils.get_visual_start_location()
+					end
+
+					local status_internals_ok, internals = pcall(require, "ts_context_commentstring.internals")
+					if not status_internals_ok then
+						return
+					end
+
+					return internals.calculate_commentstring({
+						key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+						location = location,
+					})
+				end,
+			})
 		end,
 	},
 
@@ -111,7 +168,7 @@ return {
 			disable_filetype = { "TelescopePrompt", "spectre_panel" },
 			fast_wrap = {
 				map = "<M-e>",
-				chars = { "{", "[", "(", '"', "'", "`", "<"},
+				chars = { "{", "[", "(", '"', "'", "`", "<" },
 				pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
 				offset = 0, -- Offset from pattern match
 				end_key = "$",
@@ -120,7 +177,69 @@ return {
 				highlight = "PmenuSel",
 				highlight_grey = "LineNr",
 			},
-		}, -- this is equalent to setup({}) function
+		},
+	},
+
+	-- toggleterm
+	{
+		"akinsho/toggleterm.nvim",
+		event = "VeryLazy",
+		version = "*",
+		opts = {
+			size = 20,
+			open_mapping = [[<c-\>]],
+			hide_numbers = true,
+			shade_filetypes = {},
+			shade_terminals = true,
+			shading_factor = 2,
+			start_in_insert = true,
+			insert_mappings = true,
+			persist_size = true,
+			direction = "float",
+			close_on_exit = true,
+			shell = vim.o.shell,
+			float_opts = {
+				border = "curved",
+				winblend = 0,
+				highlights = {
+					border = "Normal",
+					background = "Normal",
+				},
+			},
+		},
+		config = function(_, opts)
+			require("toggleterm").setup(opts)
+			local Terminal = require("toggleterm.terminal").Terminal
+			local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+			function _LAZYGIT_TOGGLE()
+				lazygit:toggle()
+			end
+
+			local node = Terminal:new({ cmd = "node", hidden = true })
+
+			function _NODE_TOGGLE()
+				node:toggle()
+			end
+
+			local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
+
+			function _NCDU_TOGGLE()
+				ncdu:toggle()
+			end
+
+			local htop = Terminal:new({ cmd = "htop", hidden = true })
+
+			function _HTOP_TOGGLE()
+				htop:toggle()
+			end
+
+			local python = Terminal:new({ cmd = "python", hidden = true })
+
+			function _PYTHON_TOGGLE()
+				python:toggle()
+			end
+		end,
 	},
 
 	--{
