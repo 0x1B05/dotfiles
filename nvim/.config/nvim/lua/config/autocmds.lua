336ad12468bf0bcf561caead55ad4e088d67000a
0x1B05 <liuheihei677@163.com>
2023-10-09 21:13:43 +0800

    Add nvim-metals.

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -88,6 +88,20 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	group = augroup("auto_format_on_save"),
 	desc = "Autoformat code on save",
 })
+
+-- Autocmd that will actually be in charging of starting the whole thing
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+local metals_config = require("metals").bare_config()
+vim.api.nvim_create_autocmd("FileType", {
+	-- NOTE: You may or may not want java included here. You will need it if you
+	-- want basic Java support but it may also conflict if you are using
+	-- something like nvim-jdtls which also works on a java filetype autocmd.
+	pattern = { "scala", "sbt", "java" },
+	callback = function()
+		require("metals").initialize_or_attach(metals_config)
+	end,
+	group = nvim_metals_group,
+})
 -- auto-save
 -- local function save()
 --   local buf = vim.api.nvim_get_current_buf()
--- nvim/.config/nvim/lua/plugins/05_cmp.lua
+++ nvim/.config/nvim/lua/plugins/05_cmp.lua
@@ -173,4 +173,16 @@ return {
 			}
 		end,
 	},
+	{
+		"scalameta/nvim-metals",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local metals_config = require("metals").bare_config()
+			metals_config.settings = {
+				showImplicitArguments = true,
+				excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+			}
+			metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+		end,
+	},
 }
