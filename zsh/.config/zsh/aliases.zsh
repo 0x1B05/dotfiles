e0d420637852d1dba6a334d6f4702ac50f99629e
0x1B05 <liuheihei677@163.com>
2023-10-09 18:34:38 +0800

    Add cp to "cp -r" aliase.

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,9 +3,10 @@ alias py="python3"
 alias la="ls -a"
 alias glog="git log --all --oneline --pretty=format:'%C(auto)%h%C(blue) %<|(19)%as%C(auto)%d %s' --graph"
 alias ll="ls -lhk"
-alias mv="mv -i"           # -i prompts before overwrite
-alias mkdir="mkdir -p"     # -p make parent dirs as needed
-alias df="df -h"           # -h prints human readable format
+alias mv="mv -i"
+alias mkdir="mkdir -p"
+alias cp="cp -r"
+alias df="df -h"
 alias c="clear"
 alias t="tmux"
 alias ta="tmux a"
