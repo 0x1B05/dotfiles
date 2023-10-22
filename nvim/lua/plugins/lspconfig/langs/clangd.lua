68257c5c21d842f2ee7ac7ac07bca9e761c1afe5
0x1B05 <liuheihei677@163.com>
2023-10-22 10:37:06 +0800

    Fix clangd multiple different client offset_encodings.

--- nvim/lua/plugins/lspconfig/langs/clangd.lua
+++ nvim/lua/plugins/lspconfig/langs/clangd.lua
@@ -40,6 +40,23 @@ local default_capabilities = {
 }
 
 return {
+	cmd = {
+		"clangd",
+		-- "--all-scopes-completion",
+		-- "--suggest-missing-includes",
+		-- "--background-index",
+		-- "--pch-storage=disk",
+		-- "--cross-file-rename",
+		-- "--log=info",
+		-- "--completion-style=detailed",
+		-- "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+		-- "--clang-tidy",
+		"--offset-encoding=utf-16",
+		-- "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
+		-- "--fallback-style=Google",
+		-- "--header-insertion=never",
+		-- "--query-driver=<list-of-white-listed-complers>"
+	},
 	default_config = {
 		cmd = { "clangd" },
 		filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
