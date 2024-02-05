5d6d12e60feb35a4aac758d4268fb4d63864dbb8
0x1B05 <liuheihei677@163.com>
2024-02-05 13:35:09 +0800

    Add zathura config.

--- stow.sh
+++ stow.sh
@@ -2,6 +2,7 @@ mkdir ~/.config/lazygit && stow --target=$HOME/.config/lazygit lazygit
 mkdir ~/.config/lf && stow --target=$HOME/.config/lf lf
 mkdir ~/.config/mpv && stow --target=$HOME/.config/mpv mpv
 mkdir ~/.config/mpd && stow --target=$HOME/.config/mpd mpd
+mkdir ~/.config/zathura && stow --target=$HOME/.config/zathura zathura
 
 stow --target=$HOME/.config chromium
 stow --target=$HOME/.ssh/ ssh
