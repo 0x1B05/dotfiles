4ab4b22edd2b4c1599c650f75fc2b1973160a4fd
0x1B05 <liuheihei677@163.com>
2024-04-29 16:23:17 +0800

    Update sddm.

--- sddm/scripts/wallpaper.sh
+++ sddm/scripts/wallpaper.sh
@@ -5,20 +5,20 @@
 # | |_| | |_) | (_| | (_| | ||  __/ \__ \ (_| | (_| | | | | | | 
 #  \___/| .__/ \__,_|\__,_|\__\___| |___/\__,_|\__,_|_| |_| |_| 
 #       |_|                                                     
-#  
-# by Stephan Raabe (2023) 
 # ----------------------------------------------------- 
-
 cache_file="$HOME/.cache/current_wallpaper"
+sleep 1
 clear
-echo "Update the background wallpaper of sddm to the current wallpaper."
-echo ""
+figlet "Set Wallpaper"
+echo
+echo "Set the current wallpaper as SDDM wallpaper."
+echo
 if [ ! -d /etc/sddm.conf.d/ ]; then
     sudo mkdir /etc/sddm.conf.d
     echo "Folder /etc/sddm.conf.d created."
 fi
 
-sudo cp sddm.conf /etc/sddm.conf.d/
+sudo cp $HOME/dotfiles/sddm/sddm.conf /etc/sddm.conf.d/
 echo "File /etc/sddm.conf.d/sddm.conf updated."
 
 current_wallpaper=$(cat "$cache_file")
@@ -27,10 +27,10 @@ extension="${current_wallpaper##*.}"
 sudo cp $current_wallpaper /usr/share/sddm/themes/sugar-candy/Backgrounds/current_wallpaper.$extension
 echo "Current wallpaper copied into /usr/share/sddm/themes/sugar-candy/Backgrounds/"
 new_wall=$(echo $current_wallpaper | sed "s|$HOME/wallpaper/||g")
-sudo cp theme.conf /usr/share/sddm/themes/sugar-candy/
+sudo cp $HOME/dotfiles/sddm/theme.conf /usr/share/sddm/themes/sugar-candy/
 sudo sed -i 's/CURRENTWALLPAPER/'"current_wallpaper.$extension"'/' /usr/share/sddm/themes/sugar-candy/theme.conf
 
 echo "File theme.conf updated in /usr/share/sddm/themes/sugar-candy/"
 
-echo ""
 echo "DONE! Please logout to test sddm."
+sleep 3
