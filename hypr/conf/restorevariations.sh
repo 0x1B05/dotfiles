9e2a6d1f31d60d8ec51472f19003fa50786214ed
0x1B05 <liuheihei677@163.com>
2024-05-11 18:03:14 +0800

    Update hyprland.

--- hypr/conf/restorevariations.sh
+++ hypr/conf/restorevariations.sh
@@ -22,14 +22,8 @@ echo "Your customized variation will not be overwritten or deleted."
 if gum confirm "Do you want to restore all variations to the default values?" ;then
     echo 
 
-    echo "source = ~/dotfiles/hypr/conf/keybindings/default.conf" > ~/dotfiles/hypr/conf/keybinding.conf
-    echo "Hyprland keybinding.conf restored!"
-
     echo "source = ~/dotfiles/hypr/conf/environments/default.conf" > ~/dotfiles/hypr/conf/environment.conf
     echo "Hyprland environment.conf restored!"
-
-    echo "source = ~/dotfiles/hypr/conf/windowrules/default.conf" > ~/dotfiles/hypr/conf/windowrule.conf
-    echo "Hyprland windowrule.conf restored!"
 
     echo "source = ~/dotfiles/hypr/conf/animations/default.conf" > ~/dotfiles/hypr/conf/animation.conf
     echo "Hyprland animation.conf restored!"
@@ -43,7 +37,7 @@ if gum confirm "Do you want to restore all variations to the default values?" ;t
     echo "source = ~/dotfiles/hypr/conf/monitors/default.conf" > ~/dotfiles/hypr/conf/monitor.conf
     echo "Hyprland monitor.conf restored!"
 
-    echo 
+    echo
     echo ":: Restore done!"
 else
     echo ":: Restore canceled!"
