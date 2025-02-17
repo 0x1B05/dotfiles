2ba46e7bd591994dfbe3315e84889110a48421e8
0x1B05 <liuheihei677@163.com>
2025-02-17 19:52:07 +0800

    Update alias for risc-v.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,6 +12,12 @@ alias t="tmux"
 alias ta="tmux a"
 alias lg="lazygit"
 
-alias r=trash
-alias rl='ls ~/.trash/'
-alias ur=undelfile
+# RISC-V
+alias rv32gcc='riscv64-linux-gnu-gcc -march=rv32i_zicsr -mabi=ilp32'
+alias rv32ld='riscv64-linux-gnu-ld -melf32lriscv'
+alias rv64gcc='riscv64-linux-gnu-gcc -march=rv64i_zicsr -mabi=lp64'
+alias rv64ld='riscv64-linux-gnu-ld -melf64lriscv'
+alias rv32as='riscv64-linux-gnu-as -march=rv32i_zicsr'
+alias rv64as='riscv64-linux-gnu-as -march=rv64i_zicsr'
+alias rv32objdump='riscv64-linux-gnu-objdump -d -M numeric,no-aliases'
+alias rv64objdump='riscv64-linux-gnu-objdump -d -M numeric,no-aliases'
