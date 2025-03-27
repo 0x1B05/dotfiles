e653bc22d1b1f12e5ebf4161eacffb4cc97a9000
0x1B05 <liuheihei677@163.com>
2025-03-27 09:02:38 +0800

    nvim: delete useless snippet

--- nvim/lua/plugins/luasnip/markdown/math.lua
+++ nvim/lua/plugins/luasnip/markdown/math.lua
@@ -5,6 +5,6 @@ local line_begin = require("luasnip.extras.expand_conditions").line_begin
 local tex = require("util.latex")
 
 return {
-	s({ trig = "ii", snippetType = "autosnippet" }, fmta("$<>$", i(1))),
-	s({ trig = "dd", snippetType = "autosnippet" }, fmta("$$\n<>\n$$", i(1))),
+	-- s({ trig = "ii", snippetType = "autosnippet" }, fmta("$<>$", i(1))),
+	-- s({ trig = "dd", snippetType = "autosnippet" }, fmta("$$\n<>\n$$", i(1))),
 }
