681d5833a2ba5b325ecbedecf000895d1f25bfbf
0x1B05 <liuheihei677@163.com>
2025-08-14 09:06:18 +0800

    Update hypr.

--- hypr/scripts/init-wallpaper-engine.sh
+++ hypr/scripts/init-wallpaper-engine.sh
@@ -7,7 +7,7 @@ if [ "$wallpaper_engine" == "swww" ] ;then
     swww-daemon --format xrgb
     sleep 0.5
     ~/dotfiles/hypr/scripts/wallpaper.sh init
-elif [ "$wallpaper_engine" == "hyprpaper" ] ;then    
+elif [ "$wallpaper_engine" == "hyprpaper" ] ;then
     # hyprpaper
     echo ":: Using hyprpaper"
     sleep 0.5
