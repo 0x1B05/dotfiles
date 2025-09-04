d84bade06f12475b7a63fcb18f14194af15bbe1f
0x1B05 <liuheihei677@163.com>
2025-09-04 18:44:05 +0800

    Modify font size.

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -20,15 +20,15 @@ threshhold_red=100
 # Calculate available updates pacman and aur (with trizen)
 # ----------------------------------------------------- 
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-    updates_arch=0
+if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
+    updates=0
 fi
 
-if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-    updates_aur=0
-fi
+# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+#     updates_aur=0
+# fi
 
-updates=$(("$updates_arch" + "$updates_aur"))
+# updates=$(("$updates_arch" + "$updates_aur"))
 
 # ----------------------------------------------------- 
 # Testing
