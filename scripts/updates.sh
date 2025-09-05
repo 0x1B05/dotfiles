2ab50fae17958279e880e0d915bde85893d9f5ad
0x1B05 <liuheihei677@163.com>
2025-09-05 14:17:47 +0800

    Delete useless scripts.

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -17,18 +17,18 @@ threshhold_yellow=25
 threshhold_red=100
 
 # ----------------------------------------------------- 
-# Calculate available updates pacman and aur (with trizen)
+# Calculate available updates pacman and aur
 # ----------------------------------------------------- 
 
-if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates=0
 fi
 
-# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-#     updates_aur=0
-# fi
+if ! updates_aur=$(paru -Qua 2> /dev/null | wc -l); then
+    updates_aur=0
+fi
 
-# updates=$(("$updates_arch" + "$updates_aur"))
+updates=$(("$updates_arch" + "$updates_aur"))
 
 # ----------------------------------------------------- 
 # Testing
