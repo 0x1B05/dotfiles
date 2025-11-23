3a027032c20d9d4172309a75696db472cd6b007a
0x1B05 <liuheihei677@163.com>
2025-11-23 17:50:41 +0800

    Update screenshot.sh

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -3,25 +3,39 @@
 DIR="$HOME/Pictures/screenshots/"
 NAME="screenshot_$(date +%d%m%Y_%H%M%S).png"
 
-option2="Selected area"
-option3="Fullscreen (delay 3 sec)"
+option1="Selected area"
+option2="Pin to screen (Selected area)"
+option3="Fullscreen"
 
-options="$option2\n$option3"
+options="$option1\n$option2\n$option3"
 
-choice=$(echo -e "$options" | rofi -dmenu -replace -config ~/dotfiles/rofi/config-screenshot.rasi -i -no-show-icons -l 2 -width 30 -p "Take Screenshot")
+choice=$(echo -e "$options" | rofi -dmenu -replace -config ~/dotfiles/rofi/config-screenshot.rasi -i -no-show-icons -l 3 -width 30 -p "Take Screenshot")
 
 case $choice in
-    $option2)
-        grim -g "$(slurp)" "$DIR$NAME"
-        xclip -selection clipboard -t image/png -i "$DIR$NAME"
+$option1)
+    grim -g "$(slurp)" "$DIR$NAME"
+    if [ -f "$DIR$NAME" ]; then
+        # wl-copy <"$DIR$NAME"
         notify-send "Screenshot created and copied to clipboard" "Mode: Selected area"
         swappy -f "$DIR$NAME"
+    fi
     ;;
-    $option3)
-        sleep 3
-        grim "$DIR$NAME" 
-        xclip -selection clipboard -t image/png -i "$DIR$NAME"
+$option2)
+    grim -g "$(slurp)" "$DIR$NAME"
+    if [ -f "$DIR$NAME" ]; then
+        # wl-copy <"$DIR$NAME"
+        notify-send "Screenshot pinned" "Image is pinned to screen"
+        cat "$DIR$NAME" | swayimg --class "swayimg_pin" --size=image - &
+    fi
+    ;;
+$option3)
+    sleep 0.5
+    if grim "$DIR$NAME"; then
+        # wl-copy <"$DIR$NAME"
         notify-send "Screenshot created and copied to clipboard" "Mode: Fullscreen"
         swappy -f "$DIR$NAME"
+    else
+        notify-send -u critical "Screenshot Failed" "Something went wrong"
+    fi
     ;;
 esac
