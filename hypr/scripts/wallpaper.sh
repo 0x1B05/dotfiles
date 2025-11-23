4d8b42cd9e6a95d5fd738f8d3049f89a11ded11d
0x1B05 <liuheihei677@163.com>
2025-11-23 15:18:07 +0800

    Delete swww.

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -10,98 +10,83 @@ blur="50x30"
 blur=$(cat $blur_file)
 
 # Create cache file if not exists
-if [ ! -f $cache_file ] ;then
+if [ ! -f $cache_file ]; then
     touch $cache_file
-    echo "$HOME/Beauti/wallpaper/default.jpg" > "$cache_file"
+    echo "$HOME/Beauti/wallpaper/default.jpg" >"$cache_file"
 fi
 
 # Create rasi file if not exists
-if [ ! -f $rasi_file ] ;then
+if [ ! -f $rasi_file ]; then
     touch $rasi_file
-    echo "* { current-image: url(\"$HOME/Beauti/wallpaper/default.jpg\", height); }" > "$rasi_file"
+    echo "* { current-image: url(\"$HOME/Beauti/wallpaper/default.jpg\", height); }" >"$rasi_file"
 fi
 
 current_wallpaper=$(cat "$cache_file")
 
 case $1 in
 
-    # Load wallpaper from .cache of last session 
-    "init")
-        sleep 1
-        if [ -f $cache_file ]; then
-            wal -q -i $current_wallpaper
-        else
-            wal -q -i ~/Beauti/wallpaper/
-        fi
-    ;;
-
-    # Select wallpaper with rofi
-    "select")
-        sleep 0.2
-        selected=$( find "$HOME/Beauti/wallpaper" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | sort -R | while read rfile
-        do
-            echo -en "$rfile\x00icon\x1f$HOME/Beauti/wallpaper/${rfile}\n"
-        done | rofi -dmenu -i -replace -config ~/dotfiles/rofi/config-wallpaper.rasi)
-        if [ ! "$selected" ]; then
-            echo "No wallpaper selected"
-            exit
-        fi
-        wal -q -i ~/Beauti/wallpaper/$selected
-    ;;
-
-    # Randomly select wallpaper 
-    *)
+# Load wallpaper from .cache of last session
+"init")
+    sleep 1
+    if [ -f $cache_file ]; then
+        wal -q -i $current_wallpaper
+    else
         wal -q -i ~/Beauti/wallpaper/
+    fi
+    ;;
+
+# Select wallpaper with rofi
+"select")
+    sleep 0.2
+    selected=$(find "$HOME/Beauti/wallpaper" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | sort -R | while read rfile; do
+        echo -en "$rfile\x00icon\x1f$HOME/Beauti/wallpaper/${rfile}\n"
+    done | rofi -dmenu -i -replace -config ~/dotfiles/rofi/config-wallpaper.rasi)
+    if [ ! "$selected" ]; then
+        echo "No wallpaper selected"
+        exit
+    fi
+    wal -q -i ~/Beauti/wallpaper/$selected
+    ;;
+
+# Randomly select wallpaper
+*)
+    wal -q -i ~/Beauti/wallpaper/
     ;;
 
 esac
 
-# ----------------------------------------------------- 
+# -----------------------------------------------------
 # Load current pywal color scheme
-# ----------------------------------------------------- 
+# -----------------------------------------------------
 source "$HOME/.cache/wal/colors.sh"
 echo ":: Wallpaper: $wallpaper"
 
-# ----------------------------------------------------- 
+# -----------------------------------------------------
 # get wallpaper image name
-# ----------------------------------------------------- 
+# -----------------------------------------------------
 newwall=$(echo $wallpaper | sed "s|$HOME/Beauti/wallpaper/||g")
 
-# ----------------------------------------------------- 
+# -----------------------------------------------------
 # Reload waybar with new colors
 # -----------------------------------------------------
 ~/dotfiles/waybar/launch.sh
 
-# ----------------------------------------------------- 
+# -----------------------------------------------------
 # Set the new wallpaper
 # -----------------------------------------------------
 transition_type="wipe"
 # transition_type="outer"
 # transition_type="random"
 
-wallpaper_engine=$(cat $HOME/dotfiles/.settings/wallpaper-engine.sh)
-if [ "$wallpaper_engine" == "swww" ] ;then
-    # swww
-    echo ":: Using swww"
-    swww img $wallpaper \
-        --transition-bezier .43,1.19,1,.4 \
-        --transition-fps=60 \
-        --transition-type=$transition_type \
-        --transition-duration=0.7 \
-        --transition-pos "$( hyprctl cursorpos )"
-elif [ "$wallpaper_engine" == "hyprpaper" ] ;then
-    # hyprpaper
-    echo ":: Using hyprpaper"
-    killall hyprpaper
-    wal_tpl=$(cat $HOME/dotfiles/.settings/hyprpaper.tpl)
-    output=${wal_tpl//WALLPAPER/$wallpaper}
-    echo "$output" > $HOME/dotfiles/hypr/hyprpaper.conf
-    hyprpaper &
-else
-    echo ":: Wallpaper Engine disabled"
-fi
+# hyprpaper
+echo ":: Using hyprpaper"
+killall hyprpaper
+wal_tpl=$(cat $HOME/dotfiles/.settings/hyprpaper.tpl)
+output=${wal_tpl//WALLPAPER/$wallpaper}
+echo "$output" >$HOME/dotfiles/hypr/hyprpaper.conf
+hyprpaper &
 
-if [ "$1" == "init" ] ;then
+if [ "$1" == "init" ]; then
     echo ":: Init"
 else
     sleep 1
@@ -109,10 +94,10 @@ else
     sleep 2
 fi
 
-# ----------------------------------------------------- 
+# -----------------------------------------------------
 # Created blurred wallpaper
 # -----------------------------------------------------
-if [ "$1" == "init" ] ;then
+if [ "$1" == "init" ]; then
     echo ":: Init"
 else
     dunstify "Creating blurred version ..." "with image $newwall" -h int:value:66 -h string:x-dunst-stack-tag:wallpaper
@@ -120,22 +105,22 @@ fi
 
 magick $wallpaper -resize 75% $blurred
 echo ":: Resized to 75%"
-if [ ! "$blur" == "0x0" ] ;then
+if [ ! "$blur" == "0x0" ]; then
     magick $blurred -blur $blur $blurred
     echo ":: Blurred"
 fi
 
-# ----------------------------------------------------- 
+# -----------------------------------------------------
 # Write selected wallpaper into .cache files
-# ----------------------------------------------------- 
-echo "$wallpaper" > "$cache_file"
-echo "* { current-image: url(\"$blurred\", height); }" > "$rasi_file"
+# -----------------------------------------------------
+echo "$wallpaper" >"$cache_file"
+echo "* { current-image: url(\"$blurred\", height); }" >"$rasi_file"
 
-# ----------------------------------------------------- 
+# -----------------------------------------------------
 # Send notification
-# ----------------------------------------------------- 
+# -----------------------------------------------------
 
-if [ "$1" == "init" ] ;then
+if [ "$1" == "init" ]; then
     echo ":: Init"
 else
     dunstify "Wallpaper procedure complete!" "with image $newwall" -h int:value:100 -h string:x-dunst-stack-tag:wallpaper
