b178a1f57cf6402513d263141929e876807d149e
0x1B05 <liuheihei677@163.com>
2025-11-28 20:32:29 +0800

    Update screenshot.sh.

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -9,23 +9,40 @@ option3="Fullscreen"
 
 options="$option1\n$option2\n$option3"
 
+set_opacity_full() {
+    hyprctl keyword decoration:active_opacity 1
+    sleep 0.2
+}
+
+reset_opacity() {
+    hyprctl keyword decoration:active_opacity 0.9
+}
+
 choice=$(echo -e "$options" | rofi -dmenu -replace -config ~/dotfiles/rofi/config-screenshot.rasi -i -no-show-icons -l 3 -width 30 -p "Take Screenshot")
 
 case $choice in
 $option1)
-    grim -g "$(slurp)" "$DIR$NAME"
-    if [ -f "$DIR$NAME" ]; then
-        # wl-copy <"$DIR$NAME"
-        notify-send "Screenshot created and copied to clipboard" "Mode: Selected area"
-        swappy -f "$DIR$NAME"
+    GEOM=$(slurp)
+    if [ -n "$GEOM" ]; then
+        set_opacity_full
+        grim -g "$GEOM" "$DIR$NAME"
+        reset_opacity
+        if [ -f "$DIR$NAME" ]; then
+            notify-send "Screenshot created and copied to clipboard" "Mode: Selected area"
+            swappy -f "$DIR$NAME"
+        fi
     fi
     ;;
 $option2)
-    grim -g "$(slurp)" "$DIR$NAME"
-    if [ -f "$DIR$NAME" ]; then
-        # wl-copy <"$DIR$NAME"
-        notify-send "Screenshot pinned" "Image is pinned to screen"
-        cat "$DIR$NAME" | swayimg --config=info.show=no --class "swayimg_pin" --size=image - &
+    GEOM=$(slurp)
+    if [ -n "$GEOM" ]; then
+        set_opacity_full
+        grim -g "$GEOM" "$DIR$NAME"
+        reset_opacity
+        if [ -f "$DIR$NAME" ]; then
+            notify-send "Screenshot pinned" "Image is pinned to screen"
+            cat "$DIR$NAME" | swayimg --config=info.show=no --class "swayimg_pin" --size=image - &
+        fi
     fi
     ;;
 $option3)
