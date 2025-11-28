95bec3b7c11c0881b1329fbc9d0bd1243b62c2f0
0x1B05 <liuheihei677@163.com>
2025-11-28 14:22:54 +0800

    Delete jj as esc.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -16,7 +16,6 @@ map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 map({ "n", "v", "o" }, "H", "^", { desc = "Use 'H' as '^'" })
 map({ "n", "v", "o" }, "L", "$", { desc = "Use 'L' as '$'" })
-map("i", "jj", "<Esc>")
 map("v", "p", "P")
 
 -- windows
