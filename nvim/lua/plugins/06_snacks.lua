13c61b8f7b8c125e36b859232c98e03e9a3865d5
0x1B05 <liuheihei677@163.com>
2026-01-18 21:44:02 +0800

    Update snacks.

--- nvim/lua/plugins/06_snacks.lua
+++ nvim/lua/plugins/06_snacks.lua
@@ -1,14 +1,14 @@
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    ---@type snacks.Config
-    opts = {
-        bigfile = { enabled = true },
-        dashboard = {
-            enabled = true,
-            preset = {
-                header = [[
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = {
+			enabled = true,
+			preset = {
+				header = [[
                             ⡿⠉⠄⠄⠄⠄⠈⠙⠿⠟⠛⠉⠉⠉⠄⠄⠄⠈⠉⠉⠉⠛⠛⠻⢿⣿⣿⣿⣿⣿                        
                             ⠁⠄⠄⠄⢀⡴⣋⣵⣮⠇⡀⠄⠄⠄⠄⠄⠄⢀⠄⠄⠄⡀⠄⠄⠄⠈⠛⠿⠋⠉                        
                             ⠄⠄⠄⢠⣯⣾⣿⡿⣳⡟⣰⣿⣠⣂⡀⢀⠄⢸⡄⠄⢀⣈⢆⣱⣤⡀⢄⠄⠄⠄                        
@@ -24,104 +24,290 @@ return {
 █   █▄▄▄█    ▄▄▄█  █▄█  █   ⣿⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣾⣿⣿⡟⢀⠃⠄⢸⡄⠁⣸   █       █   █       █
 █       █   █▄▄▄█       █   ⣿⠄⠄⠘⢿⣿⣿⣿⣿⣿⣿⢛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⢄⡆⠄⢀⣪⡆⠄⣿    █     ██   █ ██▄██ █
 █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█   ⡟⠄⠄⠄⠄⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣟⣻⣩⣾⣃⣴⣿⣿⡇⠸⢾     █▄▄▄█ █▄▄▄█▄█   █▄█]],
-                keys = {
-                    { icon = " ", key = "f", desc = "Find file", action = ":lua Snacks.picker.files()" },
-                    { icon = " ", key = "e", desc = "New file", action = ":ene | startinsert" },
-                    { icon = " ", key = "p", desc = "Find project", action = ":lua Snacks.picker.projects()" },
-                    { icon = " ", key = "r", desc = "Recently used files", action = ":lua Snacks.picker.recent()" },
-                    { icon = "󱎸 ", key = "t", desc = "Find text", action = ":lua Snacks.picker.grep()" },
-                    -- 这里把 Config 改为直接搜索配置目录，比打开 init.lua 更实用
-                    { icon = " ", key = "c", desc = "Configuration", action = ":lua Snacks.picker.files({cwd = vim.fn.stdpath('config')})" },
-                    { icon = " ", key = "q", desc = "Quit Neovim", action = ":qa" },
-                },
-            },
-            -- 布局设置
-            sections = {
-                { section = "header" },
-                { section = "keys", gap = 1, padding = 1 },
-                { section = "startup" },
-            },
-        },
-        explorer = { enabled = true }, -- 侧边栏文件树，替代 neo-tree/nvim-tree
-        indent = { enabled = true },   -- 缩进线，替代 indent-blankline
-        input = { enabled = true },    -- 更好的 vim.ui.input
-        picker = { enabled = true },   -- 替代 Telescope
-        notifier = { enabled = true }, -- 替代 nvim-notify
-        quickfile = { enabled = true },
-        scope = { enabled = true },
-        scroll = { enabled = true },
-        statuscolumn = { enabled = true },
-        words = { enabled = true },    -- 自动高亮光标下的词
-        
-        -- 样式微调
-        styles = {
-            notification = {
-                wo = { wrap = true } -- 通知自动换行
-            }
-        }
-    },
-    keys = {
-        -- ===========================
-        -- ToggleTerm 功能
-        -- ===========================
-        { "<leader>tg", function() Snacks.lazygit() end, desc = "Lazygit" },
-        { "<leader>tn", function() Snacks.terminal("ncdu") end, desc = "Ncdu" },
-        { "<leader>th", function() Snacks.terminal("htop") end, desc = "Htop" },
-        { "<c-\\>",      function() Snacks.terminal(nil, { win = { position = "float" } })  end, desc = "Toggle Terminal", mode = {"n", "t"} },
+				keys = {
+					{ icon = " ", key = "f", desc = "Find file", action = ":lua Snacks.picker.files()" },
+					{ icon = " ", key = "e", desc = "New file", action = ":ene | startinsert" },
+					{ icon = " ", key = "p", desc = "Find project", action = ":lua Snacks.picker.projects()" },
+					{ icon = " ", key = "r", desc = "Recently used files", action = ":lua Snacks.picker.recent()" },
+					{ icon = "󱎸 ", key = "t", desc = "Find text", action = ":lua Snacks.picker.grep()" },
+					-- 这里把 Config 改为直接搜索配置目录，比打开 init.lua 更实用
+					{
+						icon = " ",
+						key = "c",
+						desc = "Configuration",
+						action = ":lua Snacks.picker.files({cwd = vim.fn.stdpath('config')})",
+					},
+					{ icon = " ", key = "q", desc = "Quit Neovim", action = ":qa" },
+				},
+			},
+			-- 布局设置
+			sections = {
+				{ section = "header" },
+				{ section = "keys", gap = 1, padding = 1 },
+				{ section = "startup" },
+			},
+		},
+		explorer = { enabled = false },
+		indent = {
+			enabled = true,
+			animate = { enabled = false },
+		},
+		input = { enabled = true },
+		picker = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		scope = { enabled = true },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = true },
+		words = { enabled = true }, -- 自动高亮光标下的词
 
-        { "<c-h>", function() require("tmux").move_left() end, mode = "t", desc = "Go to Left Window" },
-        { "<c-j>", function() require("tmux").move_bottom() end, mode = "t", desc = "Go to Lower Window" },
-        { "<c-k>", function() require("tmux").move_top() end, mode = "t", desc = "Go to Upper Window" },
-        { "<c-l>", function() require("tmux").move_right() end, mode = "t", desc = "Go to Right Window" },
+		-- 样式微调
+		styles = {
+			notification = {
+				wo = { wrap = true }, -- 通知自动换行
+			},
+		},
+	},
+	keys = {
+		-- ===========================
+		-- ToggleTerm 功能
+		-- ===========================
+		{
+			"<leader>tg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		{
+			"<leader>tn",
+			function()
+				Snacks.terminal("ncdu")
+			end,
+			desc = "Ncdu",
+		},
+		{
+			"<leader>th",
+			function()
+				Snacks.terminal("htop")
+			end,
+			desc = "Htop",
+		},
+		{
+			"<c-\\>",
+			function()
+				Snacks.terminal(nil, { win = { position = "float" } })
+			end,
+			desc = "Toggle Terminal",
+			mode = { "n", "t" },
+		},
 
-        -- ===========================
-        -- Telescope 功能 (Picker)
-        -- ===========================
-        { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
-        { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep" },
-        { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
-        { "<leader>fh", function() Snacks.picker.help() end, desc = "Help Pages" },
-        { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
-        { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
-        
-        -- ===========================
-        -- LSP 相关
-        -- ===========================
-        { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
-        { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-        { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
-        { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-        { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
-        { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
-        { "<leader>sd", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
-        { "<leader>sD", function() Snacks.picker.diagnostics() end, desc = "Workspace Diagnostics" },
-        
-        -- ===========================
-        -- 其他实用工具
-        -- ===========================
-        { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
-        { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
-        { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
-        { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
-        { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
-        { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
-        { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-    },
-    init = function()
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "VeryLazy",
-            callback = function()
-                _G.dd = function(...) Snacks.debug.inspect(...) end
-                _G.bt = function() Snacks.debug.backtrace() end
-                vim.print = _G.dd 
+		{
+			"<c-h>",
+			function()
+				require("tmux").move_left()
+			end,
+			mode = "t",
+			desc = "Go to Left Window",
+		},
+		{
+			"<c-j>",
+			function()
+				require("tmux").move_bottom()
+			end,
+			mode = "t",
+			desc = "Go to Lower Window",
+		},
+		{
+			"<c-k>",
+			function()
+				require("tmux").move_top()
+			end,
+			mode = "t",
+			desc = "Go to Upper Window",
+		},
+		{
+			"<c-l>",
+			function()
+				require("tmux").move_right()
+			end,
+			mode = "t",
+			desc = "Go to Right Window",
+		},
 
-                Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-                Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-                Snacks.toggle.diagnostics():map("<leader>ud")
-                Snacks.toggle.line_number():map("<leader>ul")
-                Snacks.toggle.treesitter():map("<leader>uT")
-                Snacks.toggle.inlay_hints():map("<leader>uh")
-            end,
-        })
-    end,
+		-- ===========================
+		-- Telescope 功能 (Picker)
+		-- ===========================
+		{
+			"<leader>ff",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>fg",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>fb",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>fh",
+			function()
+				Snacks.picker.help()
+			end,
+			desc = "Help Pages",
+		},
+		{
+			"<leader>fp",
+			function()
+				Snacks.picker.projects()
+			end,
+			desc = "Projects",
+		},
+		{
+			"<leader>fr",
+			function()
+				Snacks.picker.recent()
+			end,
+			desc = "Recent",
+		},
+
+		-- ===========================
+		-- LSP 相关
+		-- ===========================
+		{
+			"gd",
+			function()
+				Snacks.picker.lsp_definitions()
+			end,
+			desc = "Goto Definition",
+		},
+		{
+			"gD",
+			function()
+				Snacks.picker.lsp_declarations()
+			end,
+			desc = "Goto Declaration",
+		},
+		{
+			"gr",
+			function()
+				Snacks.picker.lsp_references()
+			end,
+			nowait = true,
+			desc = "References",
+		},
+		{
+			"gI",
+			function()
+				Snacks.picker.lsp_implementations()
+			end,
+			desc = "Goto Implementation",
+		},
+		{
+			"gy",
+			function()
+				Snacks.picker.lsp_type_definitions()
+			end,
+			desc = "Goto T[y]pe Definition",
+		},
+		{
+			"<leader>ss",
+			function()
+				Snacks.picker.lsp_symbols()
+			end,
+			desc = "LSP Symbols",
+		},
+		{
+			"<leader>sd",
+			function()
+				Snacks.picker.diagnostics_buffer()
+			end,
+			desc = "Buffer Diagnostics",
+		},
+		{
+			"<leader>sD",
+			function()
+				Snacks.picker.diagnostics()
+			end,
+			desc = "Workspace Diagnostics",
+		},
+
+		-- ===========================
+		-- 其他实用工具
+		-- ===========================
+		{
+			"<leader>n",
+			function()
+				Snacks.picker.notifications()
+			end,
+			desc = "Notification History",
+		},
+		{
+			"<leader>z",
+			function()
+				Snacks.zen()
+			end,
+			desc = "Toggle Zen Mode",
+		},
+		{
+			"<leader>bd",
+			function()
+				Snacks.bufdelete()
+			end,
+			desc = "Delete Buffer",
+		},
+		{
+			"<leader>cR",
+			function()
+				Snacks.rename.rename_file()
+			end,
+			desc = "Rename File",
+		},
+		{
+			"<leader>gB",
+			function()
+				Snacks.gitbrowse()
+			end,
+			desc = "Git Browse",
+			mode = { "n", "v" },
+		},
+		{
+			"<leader>un",
+			function()
+				Snacks.notifier.hide()
+			end,
+			desc = "Dismiss All Notifications",
+		},
+	},
+	init = function()
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "VeryLazy",
+			callback = function()
+				_G.dd = function(...)
+					Snacks.debug.inspect(...)
+				end
+				_G.bt = function()
+					Snacks.debug.backtrace()
+				end
+				vim.print = _G.dd
+
+				Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+				Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+				Snacks.toggle.diagnostics():map("<leader>ud")
+				Snacks.toggle.line_number():map("<leader>ul")
+				Snacks.toggle.treesitter():map("<leader>uT")
+				Snacks.toggle.inlay_hints():map("<leader>uh")
+			end,
+		})
+	end,
 }
