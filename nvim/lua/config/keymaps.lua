98542205168e4d944ac50766928917dd1da902ad
0x1B05 <liuheihei677@163.com>
2026-01-19 11:45:25 +0800

    Simplify leap.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -143,6 +143,22 @@ M.leap = {
 		end,
 		desc = "Flash Treesitter",
 	},
+	{
+		"r",
+		mode = "o",
+		function()
+			require("flash").remote()
+		end,
+		desc = "Remote Flash",
+	},
+	{
+		"R",
+		mode = { "o", "x" },
+		function()
+			require("flash").treesitter_search()
+		end,
+		desc = "Treesitter Search",
+	},
 }
 
 M.lsp = {
