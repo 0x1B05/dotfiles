c19410adec7b313817a8dc22bde67a44755aa219
0x1B05 <liuheihei677@163.com>
2026-01-19 16:43:36 +0800

    Using mini.pair

--- nvim/lua/plugins/03_coding.lua
+++ nvim/lua/plugins/03_coding.lua
@@ -70,22 +70,9 @@ return {
 
 	-- auto pairs
 	{
-		"windwp/nvim-autopairs",
+		"echasnovski/mini.pairs",
 		event = "InsertEnter",
-		opts = {
-			disable_filetype = { "TelescopePrompt" },
-			fast_wrap = {
-				map = "<M-e>",
-				chars = { "{", "[", "(", '"', "'", "`", "<" },
-				pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-				offset = 0, -- Offset from pattern match
-				end_key = "$",
-				keys = "qwertyuiopzxcvbnmasdfghjkl",
-				check_comma = true,
-				highlight = "PmenuSel",
-				highlight_grey = "LineNr",
-			},
-		},
+		opts = {},
 	},
 
 	--{
