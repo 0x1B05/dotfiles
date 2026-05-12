2e4a46fefafb8e4a111a99337c1c6b314b5780d5
0x1B05 <liuheihei677@163.com>
2026-05-12 12:38:28 +0800

    migrate hyprland config to lua

---
+++ hypr/conf/monitor.lua
@@ -0,0 +1,20 @@
+hl.monitor({
+    output = "eDP-1",
+    mode = "preferred",
+    position = "0x0",
+    scale = "1",
+})
+
+hl.monitor({
+    output = "DP-1",
+    mode = "preferred",
+    position = "2560x0",
+    scale = "1",
+})
+
+-- hl.monitor({
+--     output = "DP-1",
+--     mode = "preferred",
+--     position = "4720x0",
+--     scale = "1",
+-- })
---
+++ hypr/hyprland.lua
@@ -0,0 +1,14 @@
+-- Hyprland 0.55 Lua configuration.
+-- Keep the old .conf files around for reference; Hyprland now prefers this file.
+
+require("conf.monitor")
+require("conf.autostart")
+require("conf.environment")
+require("conf.keyboard")
+require("conf.decoration")
+require("conf.layout")
+require("conf.misc")
+require("conf.keybinding")
+require("conf.windowrule")
+require("conf.layerrule")
+require("conf.animation")
