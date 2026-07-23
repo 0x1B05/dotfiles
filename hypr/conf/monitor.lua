6eb9a2b4f3c16a623db12f9d8918d51abed10a4b
0x1B05 <liuheihei677@163.com>
2026-07-23 10:10:51 +0800

    Update ssh and hyprland.

--- hypr/conf/monitor.lua
+++ hypr/conf/monitor.lua
@@ -1,13 +1,20 @@
 hl.monitor({
-    output = "eDP-1",
-    mode = "preferred",
-    position = "0x0",
-    scale = "1",
+	output = "eDP-1",
+	mode = "preferred",
+	position = "0x0",
+	scale = "1",
 })
 
 hl.monitor({
-    output = "DP-1",
-    mode = "preferred",
-    position = "auto-left",
-    scale = "1",
+	output = "DP-1",
+	mode = "preferred",
+	position = "auto-left",
+	scale = "1",
+})
+
+hl.monitor({
+	output = "HDMI-A-2",
+	mode = "preferred",
+	position = "auto-up",
+	scale = "1.07",
 })
